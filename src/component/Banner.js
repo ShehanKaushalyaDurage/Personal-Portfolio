b599f3970107b4fdab1fefd2866a549cce168fdb
shehan kaushalya <shehankaushalyadurage@gmail.com>
2024-08-09 21:46:28 +0530

    banner part done

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -9,40 +9,40 @@ export default function Banner() {
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(300 - Math.random() * 100);
     const [index, setIndex] = useState(1);
-    const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
+    const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
     const period = 2000;
 
-    useEffect (() => {
+    useEffect(() => {
         let ticker = setInterval(() => {
             tick();
-        },delta)
-        return () => {clearInterval(ticker)} 
-    },[text])
+        }, delta)
+        return () => { clearInterval(ticker) }
+    }, [text])
 
     const tick = () => {
         let i = loopNum % toRotate.length;
         let fullText = toRotate[i];
         let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-    
+
         setText(updatedText);
-    
+
         if (isDeleting) {
-          setDelta(prevDelta => prevDelta / 2);
+            setDelta(prevDelta => prevDelta / 2);
         }
-    
+
         if (!isDeleting && updatedText === fullText) {
-          setIsDeleting(true);
-          setIndex(prevIndex => prevIndex - 1);
-          setDelta(period);
+            setIsDeleting(true);
+            setIndex(prevIndex => prevIndex - 1);
+            setDelta(period);
         } else if (isDeleting && updatedText === '') {
-          setIsDeleting(false);
-          setLoopNum(loopNum + 1);
-          setIndex(1);
-          setDelta(500);
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setIndex(1);
+            setDelta(500);
         } else {
-          setIndex(prevIndex => prevIndex + 1);
+            setIndex(prevIndex => prevIndex + 1);
         }
-      }
+    }
 
     return (
         <section className='banner' id="home">
@@ -50,7 +50,7 @@ export default function Banner() {
                 <Row className='align-items-center'>
                     <Col xs={12} md={6} xl={7}>
                         <span className='tagline'>Welcome to my Portfolio</span>
-                        <h1>{`Hi I'm webdecoded`}<span className="wrap"> web developer</span></h1>
+                        <h1>{`Hi I'm webdecoded`}<span className="wrap"> {text}</span></h1>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                         <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25} /></button>
                     </Col>
