31540e3c14d8f3e3fdff4e3f0c49c18abe9ca551
shehan kaushalya <shehankaushalyadurage@gmail.com>
2024-08-10 00:54:31 +0530

    skill part open

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import NavBar from './component/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Banner from './component/Banner';
+import Skills from './component/Skills';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
+      <Skills />
     </div>
   );
 }
