9ed09c91bbcceb42c2bf0f34d91f3715f92cf413
shehan kaushalya <shehankaushalyadurage@gmail.com>
2024-08-09 15:15:31 +0530

    import css

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import { Nav, Navbar,Container  } from 'react-bootstrap';
+import { Nav, Navbar, Container } from 'react-bootstrap';
+import logo from '../assets/img/logo.svg';
 
 
 export default function NavBar() {
-  return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-    <Container>
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-         
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-  )
+    return (
+        <Navbar expand="lg" >
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img src={logo} alt="Logo" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav">
+                    <span className="navbar-toggler-icon"></span>
+                </Navbar.Toggle>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#Skills">Skills</Nav.Link>
+                        <Nav.Link href="#Projects">Projects</Nav.Link>
+
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
 }
