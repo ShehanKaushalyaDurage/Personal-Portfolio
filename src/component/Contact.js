9eebc35a65241c140afd0162f1841b343915ac5d
shehan kaushalya <shehankaushalyadurage@gmail.com>
2024-08-10 13:52:46 +0530

    npm install express nodemailer cors install

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -22,6 +22,26 @@ export default function Contact() {
         })
     }
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setButtonText("Sending...");
+        let response = await fetch("http://localhost:5000/contact", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json;charset=utf-8",
+            },
+            body: JSON.stringify(formDetails),
+        });
+        setButtonText("Send");
+        let result = await response.json();
+        setFormDetails(formInitialDetails);
+        if (result.code == 200) {
+            setStatus({ succes: true, message: 'Message sent successfully' });
+        } else {
+            setStatus({ succes: false, message: 'Something went wrong, please try again later.' });
+        }
+    };
+
 
     return (
         <section className="contact" id="contact">
@@ -54,6 +74,12 @@ export default function Contact() {
                                     <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
                                     <button type="submit"><span>{buttonText}</span></button>
                                 </Col>
+                                {
+                                    status.message &&
+                                    <Col>
+                                        <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
+                                    </Col>
+                                }
                             </Row>
                         </form>
                     </Col>
