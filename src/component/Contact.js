9dfb2cb7494027d5d3194ed7438c7b623a0a70ac
shehan kaushalya <shehankaushalyadurage@gmail.com>
2024-08-10 13:44:39 +0530

    contact form design part done

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -15,6 +15,14 @@ export default function Contact() {
     const [buttonText, setButtonText] = useState('Send');
     const [status, setStatus] = useState({});
 
+    const onFormUpdate = (category, value) => {
+        setFormDetails({
+            ...formDetails,
+            [category]: value
+        })
+    }
+
+
     return (
         <section className="contact" id="contact">
             <Container>
@@ -25,6 +33,29 @@ export default function Contact() {
                                 <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us" />
                             }
                         </TrackVisibility>
+                    </Col>
+                    <Col md={6}>
+                        <h2>Get In Touch</h2>
+                        <form>
+                            <Row>
+                                <Col size={12} sm={6} className="px-1">
+                                    <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
+                                </Col>
+                                <Col size={12} sm={6} className="px-1">
+                                    <input type="text" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)} />
+                                </Col>
+                                <Col size={12} sm={6} className="px-1">
+                                    <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
+                                </Col>
+                                <Col size={12} sm={6} className="px-1">
+                                    <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)} />
+                                </Col>
+                                <Col size={12} className="px-1">
+                                    <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
+                                    <button type="submit"><span>{buttonText}</span></button>
+                                </Col>
+                            </Row>
+                        </form>
                     </Col>
                 </Row>
             </Container>
