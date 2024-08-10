56356774465639db1ceb87a30030e8a267429a93
shehan kaushalya <shehankaushalyadurage@gmail.com>
2024-08-10 13:37:41 +0530

    npm i react-on-screen

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import contactImg from "../assets/img/contact-img.svg";
+import TrackVisibility from 'react-on-screen';
 
 export default function Contact() {
     const formInitialDetails = {
@@ -11,7 +14,22 @@ export default function Contact() {
     const [formDetails, setFormDetails] = useState(formInitialDetails);
     const [buttonText, setButtonText] = useState('Send');
     const [status, setStatus] = useState({});
-    
+
+    return (
+        <section className="contact" id="contact">
+            <Container>
+                <Row className="align-items-center">
+                    <Col md={6} size={12}>
+                        <TrackVisibility>
+                            {({ isVisible }) =>
+                                <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us" />
+                            }
+                        </TrackVisibility>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
 }
 
 
